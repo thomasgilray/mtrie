4b11fdd25ad7d12be73b84561da256834cc41de6
Thomas Gilray <thomas.gilray@gmail.com>
2018-12-28 17:36:59 -0600

    api is fully implemented; should only need to test more

--- mtrie.cpp
+++ mtrie.cpp
@@ -20,7 +20,7 @@ u64 utime()
 
 
 u32 x=123456789, y=362436069, z=521288629;
-u32 rnd()
+inline u32 rnd()
 {
   u32 t;
   x ^= x << 16;
@@ -50,12 +50,12 @@ void testrandom(mtrie<void>& mp)
         const u64 n = (((u64)rnd()) << 32) | rnd();
         mp.insert(n,(void*)1);
     }
-    for (u64 i = 0; i < 500000; ++i)
+    for (u64 i = 100000; i < 500000; ++i)
     {
         const u64 n = (((u64)rnd()) << 32) | rnd();
         mp.remove(n);
     }
-    for (u64 i = 0; i < 500000; ++i)
+    for (u64 i = 0; i < 700000; ++i)
     {
         const u64 n = (((u64)rnd()) << 32) | rnd();
         mp.find(n);
@@ -65,52 +65,63 @@ void testrandom(mtrie<void>& mp)
 
 void testseq(mtrie<void>& mp)
 {
-    for (u64 ep = 0; ep < 5; ++ep)
-        for (u64 j = 0; j < 100000; ++j)
+    const u64 epochs = 5;
+    const u64 max = 100000;
+    for (u64 ep = 0; ep < epochs; ++ep)
+        for (u64 j = 0; j < max; ++j)
         {
             const u64 i = ep*0x300000 + j;
             mp.insert(i,(void*)1);
         }
-    for (u64 ep = 1; ep < 6; ++ep)
-        for (u64 j = 0; j < 100000; ++j)
+    for (mtrie<void>::iter it(&mp); it.more(); it.advance())
+    {
+		//std::cout << "key=" << it.getkey() << "  val=" << (u64)it.getval() << std::endl;
+        if (it.getval() != (void*)1)
+			fatal("Bad value during iteration.");
+		for (u64 ep = 0; ep < epochs; ++ep)
+			if (it.getkey() >= ep*0x300000 && it.getkey() < ep*0x300000+max)
+				goto continuelab;
+		fatal("Key not in range during iteration");
+	continuelab:;
+    }
+    for (u64 ep = 1; ep < epochs; ++ep)
+        for (u64 j = 0; j < max; ++j)
         {
             const u64 i = ep*0x300000 + j;
             mp.remove(i);
-        }
-    for (u64 ep = 0; ep < 5; ++ep)
-        for (u64 j = 0; j < 100000; ++j)
+	}
+    for (u64 ep = 0; ep < epochs+2; ++ep)
+        for (u64 j = 0; j < max; ++j)
         {
             const u64 i = ep*0x300000 + j;
             void* ans = mp.find(i);
 	    if (ans == 0 && ep == 0)
-	      fatal("Should have found key.");
+	        fatal("Should have found key.");
 	    else if (ans != 0 && ep > 0)
-	      fatal("Should not have found key.");
+	        fatal("Should not have found key.");
         }
-    if (mp.getCount() != 100000)
-      fatal("Wrong key-count.");
+    if (mp.count() != max)
+        fatal("Wrong key-count.");
 }
 
 
 int main()
 {
   u64 start = utime();
-
   for (u64 i = 0; i < 3; ++i)
   {    
       mtrie<void> mp;
-      start = utime();
       testrandom(mp);   
-      std::cout << ((utime() - start)/1000000) << std::endl;
   }
-
+  std::cout << ((utime() - start)/1000000) << std::endl;
+  
+  start = utime();
   for (u64 i = 0; i < 3; ++i)
   {
       mtrie<void> mp;
-      start = utime();
       testseq(mp);
-      std::cout << ((utime() - start)/1000000) << std::endl;
   }
+  std::cout << ((utime() - start)/1000000) << std::endl;
   
   return 0;
 }
--- mtrie.h
+++ mtrie.h
@@ -9,17 +9,17 @@
 #include <iostream>
 
 
-
 template<unsigned d, typename V>
 class mtrienode
 {
     static const u64 ptrmask = 0xfffffffffffffff8;
+    static const u64 mask64 = 0xffffffffffffffff;
 
     struct KV
     {
         u64 key;
         V* val;
-        KV(const u64 key, V* const val) : key(key), val(val) {}
+    	KV(const u64 key, V* const val) : key(key), val(val) {}
     };
    
     union E
@@ -27,64 +27,107 @@ class mtrienode
         mtrienode<d+1,V>* c;
         KV* kv;
         u64 bits;
-
-        E() : bits(0) { }
-        E(const u64 bits) : bits(bits) { }
-        E(const E& e) : bits(e.bits) { }
+        
+	    E() : bits(0) { }
+	    E(const u64 bits) : bits(bits) { }
+	    E(const E& e) : bits(e.bits) { }
+        
+        ~E()
+        {
+			if (bits & 1)
+			{
+                KV* const _kv = (KV*)(bits & ptrmask);
+				delete _kv;
+				bits = 0;
+			}
+			else if (bits)
+			{
+				delete c;
+				bits = 0;
+			}
+        }
 
         inline void insert(const u64 k, const u64 path, V* const v, u64* const count)
         {
-	    if (bits & 1)
-	    {
-	        KV* const _kv = (KV*)(bits & ptrmask);
-	        if (_kv->key == k)
-   		    _kv->val = v;
-		else
-		{
-		    c = new mtrienode<d+1,V>();
+			if (bits & 1)
+			{
+				KV* const _kv = (KV*)(bits & ptrmask);
+				if (_kv->key == k)
+					_kv->val = v;
+				else
+				{
+					c = new mtrienode<d+1,V>();
                     c->insert(k, path >> 4, v, count);
-		    c->insert(_kv->key, _kv->key >> (4*d+12), _kv->val, count);
-		    delete _kv;
-		    --*count;
-		}
-	    }
-	    else if (bits)
-	      c->insert(k, path >> 4, v, count);
-	    else
-	    {
-	        kv = new KV(k,v);
-		bits |= 1;
-		++*count;
-	    }
+					c->insert(_kv->key, _kv->key >> (4*d+12), _kv->val, count);
+					delete _kv;
+					--*count;
+				}
+			}
+			else if (bits)
+				c->insert(k, path >> 4, v, count);
+			else
+			{
+				kv = new KV(k,v);
+				bits |= 1;
+				++*count;
+			}
         }
 
-      inline void remove(const u64 k, const u64 path, u64* const count)
+        inline bool remove(const u64 k, const u64 path, u64* const count)
         {
-	    if (bits & 1)
-	    {
-	        KV* const _kv = (KV*)(bits & ptrmask);
-	        if (_kv->key == k)
+			if (bits & 1)
+			{
+				KV* const _kv = (KV*)(bits & ptrmask);
+				if (_kv->key == k)
+				{
+					delete _kv;
+					bits = 0;
+					--*count;
+					return true;
+				}
+			}
+			else if (bits && c->remove(k, path >> 4, count))
+			{
+				delete c;
+				bits = 0;
+				return true;
+			}
+
+			return false;
+        }
+
+        inline V* find(const u64 k, const u64 path) const
+        {
+			if (bits & 1)
+			{
+				KV* const _kv = (KV*)(bits & ptrmask);
+				if (_kv->key == k)
+					return _kv->val;
+			}
+			else if (bits)
+				return c->find(k, path >> 4);
+			return 0;
+        }
+
+        inline u64 findnext(const u64 k, u64 path, V** const valptr) const
 		{
-		    delete _kv;
-		    bits = 0;
-		    --*count;
-		}
-	    }
-	    else if (bits)
-	      c->remove(k, path >> 4, count);
-        }
-
-        inline V* find(const u64 k, const u64 path)
-        {
-	    if (bits & 1)
-	    {
-	        KV* const _kv = (KV*)(bits & ptrmask);
-	        if (_kv->key == k)
-	    	    return _kv->val;
-	    }
-	    else if (bits)
-	        return c->find(k, path >> 4);
-	    return 0;
+            if (bits & 1)
+			{
+				KV const * const _kv = (KV*)(bits & ptrmask);
+				u64 kvpath = _kv->key >> (12+d*4);
+				path >>= 4;
+				for (; path != kvpath; (path >>= 4) && (kvpath >>= 4))
+				{
+					if ((kvpath&15) < (path&15))
+						return k;
+					else if ((kvpath&15) > (path&15))
+						break;
+				}
+				*valptr = _kv->val;
+				return _kv->key;
+			}
+			else
+				return c->findnext(k, path >> 4, valptr);
         }
     };
   
@@ -93,23 +136,57 @@ private:
 
 public:
     mtrienode()
-      : buff{0}
+	    : buff{0}
     {
     }
 
-    void insert(const u64 k, const u64 path, V* const v, u64* const count)
+    ~mtrienode()
     {
-      buff[path&15].insert(k,path,v,count);
+        for (u16 i = 0; i < 16; ++i)
+			(&buff[i])->~E();
     }
 
-    void remove(const u64 k, const u64 path, u64* const count)
+    inline void insert(const u64 k, const u64 path, V* const v, u64* const count)
     {
-        buff[path&15].remove(k,path,count);
+        buff[path&15].insert(k,path,v,count);
     }
 
-    V* find(const u64 k, const u64 path)
+    inline bool remove(const u64 k, const u64 path, u64* const count)
     {
-        buff[path&15].find(k,path);
+        if (buff[path&15].remove(k,path,count))
+		{
+			for (u16 i = 0; i < 16; ++i)
+				if (buff[i].bits)
+					return false;
+			return true;
+		}
+		else
+            return false;
+    }
+
+    inline V* find(const u64 k, const u64 path)
+    {
+        return buff[path&15].find(k,path);
+    }
+    
+    inline u64 findnext(u64 k, u64 path, V** const valptr) const
+    {
+        while(true)
+		{
+			if (buff[path&15].bits)
+			{
+				k = buff[path&15].findnext(k,path,valptr);
+				if (*valptr)
+					return k;
+			}
+			if (path&15 == 15)
+				return k;
+			else
+			{
+				path = (path&15) + 1;
+				k = (k&(mask64>>(56-d*4))) | (path<<(d*4+8));
+			}
+		}
     }
 };
 
@@ -117,34 +194,69 @@ public:
 
 template<typename V>
 class mtrienode<13,V>
-{   
-private:
+{
+    static const u64 topmask = 0x0fffffffffffffff;
+  
+ private:
     V* buff[16];  
 
-public:
-    mtrienode()
-        : buff{0}
+ public:
+ mtrienode()
+	 : buff{0}
     {
     }
     
-    void insert(const u64 k, const u64 path, V* const v, u64* const count)
+    ~mtrienode()
     {
-        if (buff[path&15] == 0)
-	    ++*count;
+    }
+    
+    inline void insert(const u64 k, const u64 path, V* const v, u64* const count)
+    {
+        if (buff[path] == 0)
+			++*count;
         buff[path] = v;
     }
 
-    void remove(const u64 k, const u64 path, u64* const count)
+    inline bool remove(const u64 k, const u64 path, u64* const count)
     {
-        if (buff[path&15])
-	    --*count;
-        buff[path] = 0;
+        if (buff[path])
+		{
+			--*count;
+            buff[path] = 0; 
+			for (u16 i = 0; i < 16; ++i)
+				if (buff[i])
+					return false;
+			return true;
+		}
+		else
+			return false;
     }
 
-    V* find(const u64 k, const u64 path)
+    inline V* find(const u64 k, const u64 path) const
     {
         if (buff[path])
     	    return buff[path];
+		else
+			return 0;
+    }
+
+    inline u64 findnext(u64 k, u64 path, V** const valptr) const
+    {
+        while(true)
+		{     
+			if (buff[path])
+			{
+				*valptr = buff[path];
+				return k;
+			}
+			if (path == 15)
+				return k;
+			else
+			{
+				++path;
+				k = (k&topmask) | (path<<60);
+			}
+		}
     }
 };
 
@@ -154,12 +266,13 @@ template<typename V>
 class mtrie
 {
     static const u64 ptrmask = 0xfffffffffffffff8;
-
+    static const u64 mask64 = 0xffffffffffffffff;
+	
     struct KV
     {
         u64 key;
         V* val;
-        KV(const u64 key, V* const val) : key(key), val(val) {}
+	KV(const u64 key, V* const val) : key(key), val(val) {}
     };
    
     union E
@@ -168,100 +281,227 @@ class mtrie
         KV* kv;
         u64 bits;
 
-        E() : bits(0) { }
-        E(const u64 bits) : bits(bits) { }
-        E(const E& e) : bits(e.bits) { }
+	E() : bits(0) { }
+	E(const u64 bits) : bits(bits) { }
+	E(const E& e) : bits(e.bits) { }
+
+        ~E()
+        {
+			if (bits & 1)
+			{
+                KV* const _kv = (KV*)(bits & ptrmask);
+				delete _kv;
+				bits = 0;
+			}
+			else if (bits)
+			{
+				delete c;
+				bits = 0;
+			}
+        }
 
         inline void insert(const u64 k, const u64 path, V* const v, u64* const count)
         {
-	    if (bits & 1)
-	    {
-	        KV* const _kv = (KV*)(bits & ptrmask);
-	        if (_kv->key == k)
-   		    _kv->val = v;
-		else
-		{
-		    c = new mtrienode<0,V>();
+			if (bits & 1)
+			{
+				KV* const _kv = (KV*)(bits & ptrmask);
+				if (_kv->key == k)
+					_kv->val = v;
+				else
+                {
+                    c = new mtrienode<0,V>();
                     c->insert(k, path >> 8, v, count);
-		    c->insert(_kv->key, _kv->key >> 8, _kv->val, count);
-		    delete _kv;
-		    --*count;
-		}
-	    }
-	    else if (bits)
-	      c->insert(k, path >> 8, v, count);
-	    else
-	    {
-	        kv = new KV(k,v);
-		bits |= 1;
-		++*count;
-	    }
+                    c->insert(_kv->key, _kv->key >> 8, _kv->val, count);
+                    delete _kv;
+                    --*count;
+                }
+            }
+            else if (bits)
+                c->insert(k, path >> 8, v, count);
+            else
+            {
+				kv = new KV(k,v);
+                bits |= 1;
+                ++*count;
+            }
         }
 
         inline void remove(const u64 k, const u64 path, u64* const count)
         {
-	    if (bits & 1)
-	    {
-	        KV* const _kv = (KV*)(bits & ptrmask);
-	        if (_kv->key == k)
-		{
-		    delete _kv;
-		    bits = 0;
-		    --*count;
-		}
-	    }
-	    else if (bits)
-	      c->remove(k, path >> 8, count);
+            if (bits & 1)
+            {
+                KV* const _kv = (KV*)(bits & ptrmask);
+                if (_kv->key == k)
+                {
+                    delete _kv;
+                    bits = 0;
+                    --*count;
+                }
+            }
+            else if (bits && c->remove(k, path >> 8, count))
+            {
+                delete c;
+                bits = 0;
+            }
         }
 
-        inline V* find(const u64 k, const u64 path)
+        inline V* find(const u64 k, const u64 path) const
         {
-	    if (bits & 1)
-	    {
-	        KV* const _kv = (KV*)(bits & ptrmask);
-	        if (_kv->key == k)
-	    	    return _kv->val;
-	    }
-	    else if (bits)
-	        return c->find(k, path >> 8);
-	    return 0;
+			if (bits & 1)
+            {
+                KV* const _kv = (KV*)(bits & ptrmask);
+                if (_kv->key == k)
+                    return _kv->val;
+            }
+            else if (bits)
+                return c->find(k, path >> 8);
+            return 0;
+        }
+
+        inline u64 findnext(const u64 k, u64 path, V** const valptr) const
+        {
+            if (bits & 1)
+            {
+				KV const * const _kv = (KV*)(bits & ptrmask);
+                u64 kvpath = _kv->key >> 8;
+				path >>= 8;
+				for (; path != kvpath; (path >>= 4) && (kvpath >>= 4))
+					if (kvpath&15 < path&15)
+						return k;
+					else if (kvpath&15 > path&15)
+						break;
+				*valptr = _kv->val;
+				return _kv->key;
+			}
+            else
+                return c->findnext(k, path >> 8, valptr);
         }
     };
   
-private:
+ private:
     // Not cache aligned well, but this is only the root node
-    u64 count; 
+    u64 kcount; 
     E buff[256];  
 
-public:
-    mtrie()
-      : count(0), buff{0}
+ public:
+ mtrie()
+	 : kcount(0), buff{0}
     {        
     }
-
-    u64 getCount()
+    
+    ~mtrie()
     {
-        return count;
+        for (u16 i = 0; i < 256; ++i)
+            (&buff[i])->~E();
+    }
+    
+    u64 count() const
+    {
+        return kcount;
     }
     
     void insert(const u64 k, V* const v)
     {
         if (v) 
-            buff[k&255].insert(k,k,v,&count);
-	else
-	    buff[k&255].remove(k,k,&count);
+            buff[k&255].insert(k,k,v,&kcount);
+        else
+            buff[k&255].remove(k,k,&kcount);
     }
-
+    
     void remove(const u64 k)
     {
-        buff[k&255].remove(k,k,&count);
+        buff[k&255].remove(k,k,&kcount);
+    }
+    
+    V* find(const u64 k) const
+    {
+        return buff[k&255].find(k,k);
+    }
+    
+    u64 findnext(u64 k, V** const valptr) const
+    {
+        *valptr = 0;
+        while(true)
+		{
+			if (buff[k&255].bits)
+			{
+				k = buff[k&255].findnext(k,k,valptr);
+				if (*valptr)
+					return k;
+			}
+			if (k&255 == 255)
+				return 0;
+			else
+				k = (k&255) + 1;
+		}
     }
 
-    V* find(const u64 k)
+    class iter
     {
-        buff[k&255].find(k,k);
-    }
+    private:
+        mtrie<V>* root;
+        u64 ckey;
+		V* cvalue; 
+	
+    public:
+	iter(mtrie<V>* const _root)
+		: root(_root)
+        {
+			ckey = root->findnext(0, &cvalue);
+			if (cvalue == 0)
+				root = 0;
+        }
+
+        bool more() const
+        {
+			return root != 0;
+        }
+
+        void advance()
+        {
+			//std::cout << std::hex << "before: " << ckey << std::endl;
+			if (root && ckey < mask64)
+			{
+				// Increment ckey, then findnext(..)
+				// (increment is NOT the same as ckey+1, as the
+				//  lowest-significant pseduobytes come first)
+				u16 d = 0;
+			    while (d < 14)
+				{
+					const u16 pos = (60-4*d);
+					const u64 shifted = ckey>>pos;
+					const u64 frbit = (1+(15&shifted))&15;
+					ckey = (ckey&(mask64^(mask64<<pos)))|((frbit|(shifted&(0xfffffffffffffff0)))<<pos);
+					if (frbit) break;
+					++d;
+				}
+				if (d==14 && ckey&0x0000000000000f00==0)
+					++ckey;
+				//std::cout << std::hex << "after: " << ckey << std::dec << std::endl;
+				// Then scan forward from position ckey
+				ckey = root->findnext(ckey,&cvalue);
+				if (cvalue == 0)
+					root = 0;
+			}
+			else
+				root = 0;
+        }
+
+        u64 getkey() const
+        {
+			return ckey;
+        }
+
+        V* getval() const
+        {
+			return cvalue;
+        } 
+    };
 };
+
+
+
+
 
 
 
